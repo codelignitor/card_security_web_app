fc26696c1868c776dc6a62e7ad6dd2239013f514
itsmehardawood <mehar.dawood.official@gmail.com>
2025-07-11 14:56:17 +0500

    Domain setups added

--- src/app/page copy.js
+++ src/app/page copy.js
@@ -3,3 +3,11 @@ import MainPage from "./components/MainPage";
 export default function HomePage() {
   return <MainPage />;
 }
+ // {
+    //   "source": "/lollicash",
+    //   "destination": "https://card-detection-businesses.vercel.app/lollicash"
+    // },
+    // {
+    //   "source": "/lollicash/:match*",
+    //   "destination": "https://card-detection-businesses.vercel.app/lollicash/:match*"
+    // }
